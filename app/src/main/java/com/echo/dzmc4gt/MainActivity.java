c038eab256e649b85aded5fdb42384df2b3c9633
kmstats <kmstats@126.com>
2024-12-21 16:56:52 +0800

    布局方式定稿

--- app/src/main/java/com/echo/dzmc4gt/MainActivity.java
+++ app/src/main/java/com/echo/dzmc4gt/MainActivity.java
@@ -8,13 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.echo.dzmc4gt.ui.reflow.ReflowFragment;
+import com.echo.dzmc4gt.ui.slideshow.SlideshowFragment;
+import com.echo.dzmc4gt.ui.transform.TransformFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -23,10 +25,11 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.appcompat.app.AppCompatActivity;
 import com.echo.dzmc4gt.databinding.ActivityMainBinding;
 
-import  androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -65,8 +68,26 @@ public class MainActivity extends AppCompatActivity {
             //设置tabLayout
             tableLayout = findViewById(R.id.tabLayout);
             viewPager = findViewById(R.id.viewPager);
-            viewPager.setAdapter(new ViewPagerAdapter(this));
-            new TabLayoutMediator(tableLayout,viewPager, (tab, position)->tab.setText("Tab "+ (position + 1))).attach();
+
+            ArrayList<Fragment> fragments = new ArrayList<>();
+            fragments.add(new ReflowFragment());
+            fragments.add(new SlideshowFragment());
+            fragments.add(new TransformFragment());
+
+            ArrayList<String> titles = new ArrayList<>();
+            titles.add(getString(R.string.unitTree));
+            titles.add(getString(R.string.stubQuery));
+            titles.add(getString(R.string.comQuery));
+
+            ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter((this),fragments);
+            viewPager.setAdapter(viewPagerAdapter);
+            TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tableLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+                @Override
+                public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                    tab.setText(titles.get(position));
+                }
+            });
+            tabLayoutMediator.attach();
         }
 
 
@@ -108,24 +129,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    public class ViewPagerAdapter extends FragmentStateAdapter {
-
-        public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
-        }
-
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            return new MyFragment();
-        }
-
-        @Override
-        public int getItemCount() {
-            return 3;
-        }
     }
 
     public static class MyFragment extends Fragment {
---
+++ app/src/main/java/com/echo/dzmc4gt/ViewPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.echo.dzmc4gt;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    private ArrayList<Fragment> fragmentList;
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity, ArrayList<Fragment> fragments) {
+        super(fragmentActivity);
+        this.fragmentList = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+}
